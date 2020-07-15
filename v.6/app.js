4583b414f46893523cd8528771896a4583d5393c
Tedla B <mekdi.td87@outlook.com>
2020-07-15 05:55:47 -0500

    added 'Authentication Method' to the app

--- v.6/app.js
+++ v.6/app.js
@@ -2,16 +2,24 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
   Campground = require("./models/campground"),
   Comment = require("./models/comment"),
-  seedDB = require("./seeds");
+  User = require("./models/user"),
+  seedDB = require("./seeds")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v5", { useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect("mongodb://localhost/yelp_camp_v6", { useNewUrlParser: true, useUnifiedTopology: true  });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 seedDB();
 
+// PASSPORT CONFIGURATION
+
+app.use(require("express-session")({
+  secret: "Lucy will always remembered!"
+}))
 
 app.get("/", function (req, res) {
   res.render("landing");
@@ -107,6 +115,33 @@ app.post("/campgrounds/:id/comments", function(req, res){
     }
   })
 })
+
+//==================
+//AUTH ROUTES
+//==================
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+//handle sign up logic
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("campgrounds");
+    });
+  });
+});
+
+
+
 
 app.listen(3004, function () {
   console.log("Server is listening on port 3004!");
